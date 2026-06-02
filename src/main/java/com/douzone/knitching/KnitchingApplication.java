7727ced2e97583e93c39ddbef7c2c7425236210c
SumiNK03 <0417nam@naver.com>
2026-06-02 16:12:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/douzone/knitching/KnitchingApplication.java
@@ -0,0 +1,13 @@
+package com.douzone.knitching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnitchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnitchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/douzone/knitching/KnitchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douzone.knitching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnitchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
